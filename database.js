6ee35a1b1a64e45c11d64349913de2d8434d0b69
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2022-09-30 09:00:28 +0630

    update

--- database.js
+++ database.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 3000000,
+    idle: 10000,
+  },
+});
 
-sequelize
-  .sync({})
-  .then(() => {})
-  .catch(console.log);
+// sequelize
+//   .sync({ force: true })
+//   .then(() => {})
+//   .catch(console.log);
 
 module.exports = sequelize;
--- hooks.js
+++ hooks.js
@@ -1,11 +1,17 @@
+const fs = require("fs");
+const moment = require("moment");
+const path = require("path");
 const sequelize = require("./database");
 const Application = require("./models/Application");
 const User = require("./models/User");
 
 exports.beforeServerStart = async () => {
   console.log("before server start.");
-
+  if (!fs.existsSync("logs")) {
+    fs.mkdirSync("logs");
+  }
   await sequelize.authenticate();
+  await sequelize.sync({});
   let [application] = await Application.findOrCreate({
     where: {
       name: process.env.APP_ID,
@@ -35,9 +41,22 @@ exports.beforeServerStart = async () => {
 
 exports.errorHandler = (err, req, res) => {
   console.error(err);
-  res.json({
+  const body = {
     code: 500,
     message: err.message,
     stack: err.stack,
-  });
+  };
+  res.status(500).json(body);
+  const now = moment().format("DD-MM-YYYY");
+  if (fs.existsSync(path.join("logs", `errors_${now}.txt`))) {
+    fs.writeFileSync(
+      path.join("logs", `errors_${now}.txt`),
+      `${err.message}\n`
+    );
+  } else {
+    fs.appendFileSync(
+      path.join("logs", `errors_${now}.txt`),
+      `${err.message}\n`
+    );
+  }
 };
